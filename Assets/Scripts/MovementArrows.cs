c07dc612799de66500e9370647aa07ae2eff22a1
Will <will.lucen4@gmail.com>
2017-10-21 12:56:54 -0300

    Blockwars started. Movement, camera, 2 player camera did

---
+++ Assets/Scripts/MovementArrows.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementArrows : MonoBehaviour {
+
+    //ajustar speed de rotação
+    [SerializeField]
+    float rotationSpeed;
+    [SerializeField]
+    float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("HorizontalArrows") != 0)
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal"));
+            print(transform.rotation);
+        }
+
+        if (Input.GetAxisRaw("VerticalArrows") != 0)
+        {
+            transform.Translate(Vector2.up * movementSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical"));
+            print(transform.position);
+        }
+    }
+}
---
+++ Assets/Scripts/MovementLetters.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementLetters : MonoBehaviour {
+
+    //ajustar speed de rotação
+    [SerializeField]
+    float rotationSpeed;
+    [SerializeField]
+    float movementSpeed;
+
+	// Use this for initialization
+	void Start () {
+        	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        if (Input.GetAxisRaw("HorizontalLetters") != 0)
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal"));
+            print(transform.rotation);
+        }
+
+        if (Input.GetAxisRaw("VerticalLetters") != 0)
+        {
+            transform.Translate(Vector2.up * movementSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical"));
+            print(transform.position);
+        }
+    }
+}
