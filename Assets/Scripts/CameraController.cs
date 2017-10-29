1fe9f5af10e9108d0e1b26b82723f993e43af5d3
Will Lucena <will.lucen4@gmail.com>
2017-10-29 10:53:01 -0300

    bugs fixed and gitignore removed to test. ready to start

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    [SerializeField]
+    GameObject target;
+    private Vector3 offset = new Vector3(0, 0, -1);
+
+    private void LateUpdate()
+    {
+        transform.position = new Vector3(
+            transform.position.x + offset.x,
+            transform.position.y + offset.y,
+            -10);
+    }
+}
